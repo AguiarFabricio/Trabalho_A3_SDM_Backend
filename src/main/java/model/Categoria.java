5d67966de2779f676cf7a14fdb2fa6d0a7c5b8c7
mariooimp <eumarioimperator@gmail.com>
2025-10-29 12:28:29 -0300

    feat(model): add atributos e métodos básicos

--- src/main/java/model/Categoria.java
+++ src/main/java/model/Categoria.java
@@ -6,4 +6,59 @@ package model;
  */
 public class Categoria {
     
+    private int id;
+    private String nome;   
+    private EmbalagemProduto embalagem;         
+    private TamanhoProduto tamanho;
+
+    public Categoria() {
+    }
+
+    public Categoria(int id, String nome, EmbalagemProduto embalagem, TamanhoProduto tamanho) {
+        this.id = id;
+        this.nome = nome;
+        this.embalagem = embalagem;
+        this.tamanho = tamanho;
+    }
+    
+    public Categoria(int idCategoria, String nomeCategoria) {
+        this.id = idCategoria;
+        this.nome = nomeCategoria;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public EmbalagemProduto getEmbalagem() {
+        return embalagem;
+    }
+
+    public void setEmbalagem(EmbalagemProduto embalagem) {
+        this.embalagem = embalagem;
+    }
+
+    public TamanhoProduto getTamanho() {
+        return tamanho;
+    }
+
+    public void setTamanho(TamanhoProduto tamanho) {
+        this.tamanho = tamanho;
+    }
+    
+    public String toString() {
+        return id + " / " + nome;
+    }
 }
