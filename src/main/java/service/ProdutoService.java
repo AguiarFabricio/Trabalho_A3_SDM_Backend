f2d7dd8a824bef13631da54ed3b64e2069bade25
Fabricio de Aguiar <aguiarfilho2006@gmail.com>
2025-10-31 09:40:03 -0300

    Merge origin/main into main
    # Conflicts:
    #	src/main/java/com/mycompany/trabalho_a3_sdm_frontend/Trabalho_A3_SDM_Frontend.java

--- src/main/java/service/ProdutoService.java
+++ src/main/java/service/ProdutoService.java
@@ -1,9 +1,22 @@
 package service;
 
-/**
- *
- * @author mario
- */
+import dao.ProdutoDAO;
+
+import dao.ProdutoDAO;
+
 public class ProdutoService {
-    
+
+    private final ProdutoDAO produtoDAO;
+
+    public ProdutoService(ProdutoDAO produtoDAO) {
+        this.produtoDAO = produtoDAO;
+    }
+
+    public boolean reajustarPrecos(double percentual) {
+        if (percentual <= 0) {
+
+            throw new IllegalArgumentException("O percentual deve ser maior que zero.");
+        }
+        return produtoDAO.reajustarPrecos(percentual);
+    }
 }
